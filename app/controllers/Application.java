fbdf7efcc3dad5b7083bcccc1437db6487d25c30
Renato Filipe Vieira <renato.filipe.vieira@gmail.com>
2014-04-09 21:48:51 +0100

    aaaah, ohhhh

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,7 +3,11 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.Index;
-import views.html.Page1;
+import views.html.Products;
+import views.html.Profile;
+import views.html.Supplier;
+import views.html.Admin;
+import views.html.Category;
 
 /**
  * Implements the controllers for this application.
@@ -22,8 +26,26 @@ public class Application extends Controller {
    * Returns page1, a simple example of a second page to illustrate navigation.
    * @return The Page1.
    */
-  public static Result page1() {
-    return ok(Page1.render("Welcome to Page1."));
+  public static Result products() {
+    return ok(Products.render("Here's some products"));
     
   }
+  
+  public static Result profile() {
+      return ok(Profile.render("Here's some profiles"));
+      
+    }
+  public static Result admin() {
+      return ok(Admin.render("Here's some admins"));
+      
+    }
+  public static Result supplier() {
+      return ok(Supplier.render("Here's some suppliers"));
+      
+    }
+
+  public static Result category(String cat) {
+      return ok(Category.render("Here's some products from " + cat));
+      
+    }
 }
